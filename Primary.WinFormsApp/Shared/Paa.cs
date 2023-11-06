06f5caf657d4e704461d5393b5668bd620959595
4 <p_armando@hotmail.com>
2023-11-06 11:58:23 -0300

    Mostramos en un message  box lo que debemos operar.

--- Primary.WinFormsApp/Shared/Paa.cs
+++ Primary.WinFormsApp/Shared/Paa.cs
@@ -1,23 +1,30 @@
 ﻿using Primary.Data;
+using Primary.Data.Orders;
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Primary.WinFormsApp
 {
     internal class Paa
     {
-        internal static Task<string> ShowComoComprarOrden(InstrumentDetail instrument, decimal value1, decimal value2)
+        internal static Task<string> ShowComoComprarOrden(InstrumentDetail instrument, decimal nominales, decimal precio)
         {
-            throw new NotImplementedException();
+            string orden = $"VENDER {nominales:#,##0} nominales de {instrument.InstrumentId.SymbolWithoutPrefix()} a {precio}";
+            MessageBox.Show(orden, "Operar esto:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return Task.Run(() => orden);
         }
 
-        internal static Task<string> ShowComoVenderOrden(InstrumentDetail instrument, decimal value1, decimal value2)
+        internal static Task<string> ShowComoVenderOrden(InstrumentDetail instrument, decimal nominales, decimal precio)
         {
-            throw new NotImplementedException();
+            string orden = $"COMPRAR {nominales:#,##0} nominales de {instrument.InstrumentId.SymbolWithoutPrefix()} a {precio}";
+            MessageBox.Show(orden, "Operar esto:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            return Task.Run(() => orden);
         }
     }
 }
